df11f0039954acb713e497524fde8bf8409d87c0
AYJ12 <95719027+AYJ12@users.noreply.github.com>
2023-09-21 17:41:20 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 1.17 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(string t, string p) {
+        int answer = 0;
+        
+        for(int i = 0; i <= t.Length - p.Length; i++){
+            string str = t.Substring(i, p.Length);
+            if(long.Parse(str) <= long.Parse(p))
+                answer++;
+        }
+        
+        return answer;
+    }
+}
